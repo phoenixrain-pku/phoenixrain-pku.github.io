27b1ed3665f766075f8d29b8b9c719508b2f0c93
林汇平 <lhp22@mails.tsinghua.edu.cn>
2023-07-23 11:52:16 +0800

    Site updated: 2023-07-23 11:52:15

--- games/js/bgset 2.js
+++
@@ -1,6 +0,0 @@
-var canvas = document.getElementById("canvas");
-var Height = window.screen.height * 0.8;
-var Width = Math.min(window.screen.height * 0.6,window.screen.width)*0.8;
-canvas.height = Height;
-canvas.width = Width;
-var ChangeBasis = 170;
